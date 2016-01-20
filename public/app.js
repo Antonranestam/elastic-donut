3bf71cd43be0a4f6b46529c9d90ac18eaee630a6
Jens Lind <jens.lind@oddhill.se>
2016-01-20 20:07:55 +0100

    Initial angular

--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
-console.log(':D')
+import angular from 'angular'
+import ngRoute from 'angular-route'
+import appRoutes = from './app.route'
+
+const app = angular.module('elasticDonut', [ngRoute])
+
+app.config(($routeProvider) => appRoutes($routeProvider))
---
+++ public/app.route.js
@@ -0,0 +1,8 @@
+const routes = function (route) {
+  route
+    .when('/', {
+      templateUrl: 'templates/front.html'
+    })
+}
+
+export default routes
