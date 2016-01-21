ea12aa3a6d738907c36cda9ac25eaa65011cce32
Jens Lind <jens.lind@oddhill.se>
2016-01-21 14:04:05 +0100

    Added ngAnimate

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
+import ngAnimate from 'angular-animate'
 import appRoutes from './app.route'
 
-const app = angular.module('elasticDonut', [ngRoute])
+const app = angular.module('elasticDonut', [ngRoute, ngAnimate])
 
 app.config(($routeProvider) => appRoutes($routeProvider))
