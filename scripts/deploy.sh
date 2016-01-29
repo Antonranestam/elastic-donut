cee9413cd7e66981875bd5d182083eebf5ba5c9a
Jens Lind <jens.lind@oddhill.se>
2016-01-29 09:49:48 +0100

    Use donut.pem

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 echo 'Deploying...'
-rsync -rave 'ssh -i ${DEPLOY_KEY}' -av --exclude-from './scripts/excluded-files.txt' ./ donut@icepick.xn--q9jyb4c:/home/donut/nodes/elastic-donut
+rsync -rave 'ssh -i ./donut.pem' -av --exclude-from './scripts/excluded-files.txt' ./ donut@icepick.xn--q9jyb4c:/home/donut/nodes/elastic-donut
 echo 'Building...'
-ssh -i $DEPLOY_KEY donut@icepick.xn--q9jyb4c 'cd /home/donut/nodes/elastic-donut && npm install && npm run build'
+ssh -i ./donut.pem donut@icepick.xn--q9jyb4c 'cd /home/donut/nodes/elastic-donut && npm install && npm run build && pm2 restart donut'
