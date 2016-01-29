8b80787bbe76d51b2159e94b95cde457518f0b95
Jens Lind <jens.lind@oddhill.se>
2016-01-29 11:58:52 +0100

    Set .pem to 600

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 echo 'Generate .pem key...'
 openssl aes-256-cbc -K $encrypted_cef7aeeb76ec_key -iv $encrypted_cef7aeeb76ec_iv -in donut.pem.enc -out donut.pem -d
+chmod 600 donut.pem
 echo 'Deploying...'
 rsync -rave 'ssh -i donut.pem -o StrictHostKeyChecking=no' -av --exclude-from './scripts/excluded-files.txt' ./ donut@icepick.xn--q9jyb4c:/home/donut/nodes/elastic-donut
 echo 'Building...'
