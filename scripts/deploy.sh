af07fc3a9d1dd793835816035c0b8834ae3eeff2
Jens Lind <jens.lind@oddhill.se>
2016-01-29 11:55:03 +0100

    Moved .pem generation to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
-pwd
-ls -al
+echo 'Generate .pem key...'
+openssl aes-256-cbc -K $encrypted_cef7aeeb76ec_key -iv $encrypted_cef7aeeb76ec_iv -in donut.pem.enc -out donut.pem -d
 echo 'Deploying...'
-rsync -rave 'ssh -i .travis/donut.pem -o StrictHostKeyChecking=no' -av --exclude-from './scripts/excluded-files.txt' ./ donut@icepick.xn--q9jyb4c:/home/donut/nodes/elastic-donut
+rsync -rave 'ssh -i donut.pem -o StrictHostKeyChecking=no' -av --exclude-from './scripts/excluded-files.txt' ./ donut@icepick.xn--q9jyb4c:/home/donut/nodes/elastic-donut
 echo 'Building...'
-ssh -i .travis/donut.pem -o StrictHostKeyChecking=no donut@icepick.xn--q9jyb4c 'cd /home/donut/nodes/elastic-donut && npm install && npm run build && pm2 restart donut'
+ssh -i donut.pem -o StrictHostKeyChecking=no donut@icepick.xn--q9jyb4c 'cd /home/donut/nodes/elastic-donut && npm install && npm run build && pm2 restart donut'
