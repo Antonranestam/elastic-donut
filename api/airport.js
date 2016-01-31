3b77430ef9153e3c2d1846f1dc3aadbcee74054e
Jens Lind <jens.lind@oddhill.se>
2016-01-31 16:03:58 +0100

    Added airport search to api [ci skip]

--- api/airport.js
+++ api/airport.js
@@ -19,6 +19,23 @@ class Airport {
       })
   }
 
+  search (term) {
+    const self = this
+
+    let lastChar = term.slice(-1)
+    let termBetween = term.slice(0, -1) + String.fromCharCode(lastChar.charCodeAt(0) + 1)
+
+    return r.table('airports_search')
+      .between(term.toLowerCase(), termBetween.toLowerCase(), { index: 'term' })
+      .limit(10)
+      .eqJoin('airportId', r.table('airports'))
+      .zip()
+      .run(self.conn)
+      .then((cursor) => {
+        return cursor.toArray()
+      })
+  }
+
   static init (req, reply) {
     if (!(airport instanceof Airport)) airport = new Airport(req.dbConn)
     reply.continue()
@@ -31,6 +48,17 @@ exports.routes = [
     path: '/api/airport/get',
     handler: (req, reply) => {
       airport.get()
+        .then((result) => reply(result))
+    },
+    config: {
+      pre: [Airport.init]
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/airport/search/{term}',
+    handler: (req, reply) => {
+      airport.search(req.params.term)
         .then((result) => reply(result))
     },
     config: {
