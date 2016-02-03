e58477caaaac21a6e17c133e5c7ba7b2c3173a78
Jens Lind <jens.lind@oddhill.se>
2016-02-03 20:49:06 +0100

    Added destination component

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,5 +1,7 @@
 import Home from '../components/Home'
+import Destination from '../components/Destination'
 
 export const appRoutes = [
-  { path: '/', component: Home, as: 'Home' }
+  { path: '/', component: Home, as: 'Home' },
+  { path: '/destination', component: Destination, as: 'Destination' }
 ]
---
+++ public/components/Destination.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import { Component, View } from 'angular2/core'
+
+@Component({
+  selector: 'destination'
+})
+@View({
+  template: 'Destination'
+})
+export default class Destination {
+
+}
